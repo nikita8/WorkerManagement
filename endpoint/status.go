db0536dd2e729d724afb1ca4f07e9b401a692ef4
Rohit Joshi <link2j.roy@gmail.com>
2018-11-08 16:49:18 +0000

    New status routes and log the request headers (main purpose is to make sure we have Amazon Trace Id)

--- endpoint/status.go
+++ endpoint/status.go
@@ -4,12 +4,22 @@ import (
 	"context"
 
 	"github.com/graniticio/granitic/ws"
+	"github.com/graniticio/granitic/logging"
 )
 
 type StatusLogic struct {
-
+	Log logging.Logger
 }
 
 func (st *StatusLogic) Process(ctx context.Context, req *ws.WsRequest, res *ws.WsResponse) {
+
+	st.Log.LogInfof("X-Amzn-Trace-Id=%s", req.UnderlyingHTTP.Request.Header.Get("X-Amzn-Trace-Id"))
+
+	for name, headers := range req.UnderlyingHTTP.Request.Header {
+		for _, h := range headers {
+			st.Log.LogInfof("[Header] %v: %v", name, h)
+		}
+	}
+
 	res.Body = "All Good!"
 }
