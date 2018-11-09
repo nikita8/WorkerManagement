af0457747b4d0aca21d2d0e27a2d3d980bb2e73a
Rohit Joshi <link2j.roy@gmail.com>
2018-11-09 11:30:52 +0000

    update service stuff

--- endpoint/status.go
+++ endpoint/status.go
@@ -12,9 +12,6 @@ type StatusLogic struct {
 }
 
 func (st *StatusLogic) Process(ctx context.Context, req *ws.WsRequest, res *ws.WsResponse) {
-
-	st.Log.LogInfof("X-Amzn-Trace-Id=%s", req.UnderlyingHTTP.Request.Header.Get("X-Amzn-Trace-Id"))
-
 	for name, headers := range req.UnderlyingHTTP.Request.Header {
 		for _, h := range headers {
 			st.Log.LogInfof("[Header] %v: %v", name, h)
